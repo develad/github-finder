430ec739f6164e815a16a908fa051d5e12dda79e
develad <develadb@gmail.com>
2022-12-06 23:19:41 +0200

    Navbar Component - Chapter 46

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div className="w-[70%]  m-auto p-16 flex flex-col text-white">
-      <h1 className="text-white text-8xl mb-20 text-center">Hello world</h1>
-      <p className="mb-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
-        necessitatibus autem nihil eveniet laborum porro, quae hic iusto itaque,
-        debitis ratione rerum! Dolores soluta at error animi adipisci unde, sed
-        nulla accusamus ea dolore necessitatibus voluptatibus consectetur nihil
-        molestias cupiditate explicabo ad sequi commodi placeat, rerum sapiente
-        magni earum? Aliquam.
-      </p>
-      <p className="mb-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
-        necessitatibus autem nihil eveniet laborum porro, quae hic iusto itaque,
-        debitis ratione rerum! Dolores soluta at error animi adipisci unde, sed
-        nulla accusamus ea dolore necessitatibus voluptatibus consectetur nihil
-        molestias cupiditate explicabo ad sequi commodi placeat, rerum sapiente
-        magni earum? Aliquam.
-      </p>
-      <p className="mb-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
-        necessitatibus autem nihil eveniet laborum porro, quae hic iusto itaque,
-        debitis ratione rerum! Dolores soluta at error animi adipisci unde, sed
-        nulla accusamus ea dolore necessitatibus voluptatibus consectetur nihil
-        molestias cupiditate explicabo ad sequi commodi placeat, rerum sapiente
-        magni earum? Aliquam.
-      </p>
-      <img
-        className="m-0 border-8 rounded-lg scale-x-[-1]"
-        src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-        alt="computer image"
-      />
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main>Content</main>
     </div>
   );
 }
