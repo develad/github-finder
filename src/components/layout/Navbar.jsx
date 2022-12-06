8e14ef809db540db3ba8a87b2ba266bc56d4178f
develad <develadb@gmail.com>
2022-12-06 23:34:44 +0200

    Navbar Component | Adding Comments - Chapter 46

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 
 function Navbar({ title }) {
   return (
-    <nav className="navbar mb-12 shadow-lg bg-neutral text-white ">
+    // navbar and text-neutral-content is a daisyui utility class
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content ">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
+          {/* align-middle === vertical-align: middle; aligning the Github icon and the title to the center */}
           <Link
             to="/"
             className="text-lg font-bold align-middle"
@@ -15,10 +17,12 @@ function Navbar({ title }) {
             {title}
           </Link>
         </div>
+        {/* Taking all the remaining available space in the container with the flex-1 property */}
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
             <Link
               to="/"
+              // btn and btn-ghost is a daisyui utility class
               className="btn btn-ghost btn-sm rounded-btn"
             >
               Home
