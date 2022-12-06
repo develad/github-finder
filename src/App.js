d9d9eda82c14f8b5e5d5334891406b74eee7bacb
develad <develadb@gmail.com>
2022-12-07 00:03:45 +0200

    Footer Component - Chapter 47

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar />
-      <main className="px-3 container pb-12 mx-auto">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque esse
-        excepturi eveniet nisi debitis voluptatem? Repellat debitis odit
-        laboriosam adipisci facilis quibusdam laborum perferendis eius
-        laudantium in sit distinctio atque, itaque sapiente dignissimos? Quia
-        dolores magnam aperiam architecto, nesciunt nemo unde ipsa neque sunt,
-        culpa reiciendis dolorum et corporis inventore sed cum facere ullam
-        exercitationem saepe adipisci, officia aut beatae? Quaerat asperiores
-        necessitatibus sint quasi nulla explicabo numquam ipsa quo incidunt
-        harum non vel perferendis, fugit voluptatem voluptate sequi? Itaque ad
-        possimus facilis iusto dignissimos nihil iste optio, laborum earum quis
-        similique repellat ipsam. Quisquam, soluta culpa quia, sint voluptatem
-        ad reprehenderit nostrum deleniti illo, amet molestias corporis aliquid
-        hic repudiandae nisi temporibus? Quis nulla, alias esse dolorem numquam
-        repudiandae odit itaque eaque laboriosam natus rem ex magnam veritatis.
-        Debitis iusto a earum nisi accusantium est, ut quae ipsa officia, dolor
-        obcaecati, magnam dignissimos minima rem repellendus enim id corrupti.
-        Maxime accusantium odit neque dolorem eveniet, similique porro
-        distinctio ipsa totam repudiandae odio pariatur nihil laboriosam ab
-        harum est veniam cum sapiente rem minus eos voluptatibus esse. Assumenda
-        repudiandae porro nihil et itaque voluptatibus, repellat magnam fugiat
-        placeat? Recusandae harum fuga tempora, quas pariatur atque. Modi
-        voluptatum nobis veniam repellat!
-      </main>
+      <main className="px-3 container pb-12 mx-auto">Content</main>
       <Footer />
     </div>
   );
