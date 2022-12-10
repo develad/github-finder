ab51be749ac5d48a7b791bbd2bd87492a79457be
develad <develadb@gmail.com>
2022-12-10 11:15:26 +0200

    User Search Component - Chapter 56

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UsersResults from "../components/users/UsersResults";
+import UsersSearch from "../components/users/UsersSearch";
 
 function Home() {
   return (
     <>
+      <UsersSearch />
       <UsersResults />
     </>
   );
